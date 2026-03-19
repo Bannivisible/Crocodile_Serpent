077fc46ed227a18a5f4ba85d650948e18bc5a553
Bannivisible <josseswan4@gmail.com>
2026-03-19 06:58:09 +0100

    on jaw

--- Components/CombatSkill/Weapon/JawTrap/Scripts/on_jaw.gd
+++ Components/CombatSkill/Weapon/JawTrap/Scripts/on_jaw.gd
@@ -8,8 +8,8 @@ extends StateMachine
 @export var remote_f_parent_path: NodePath
 @export var remote_b_parent_path: NodePath
 
-@export var remote_f_name: StringName
-@export var remote_b_name: StringName
+@export var remote_f: RemoteTransform2D
+@export var remote_b: RemoteTransform2D
 
 @export var gauge: Control
 @export_range(0.0, 999.9) var bonus_time_amount: float= 1.0
@@ -31,22 +31,39 @@ func _ready() -> void:
 	
 	dynamic_timer.timeout.connect(_on_dynamic_timer_timeout)
 	hit_box.hit.connect(_on_hit_box_hit)
+	#
+	#await object.ready
+	#
+	#var f_remote: RemoteTransform2D= get_node_or_null(String(remote_f_parent_path) + "/" + remote_f_name)
+	#if f_remote:
+		#remote_trans_f= f_remote
+	#else :
+		#remote_trans_f = _innit_remote_trans(remote_f_parent_path)
+		#remote_trans_f.name = remote_f_name
+	#
+	#var b_remote: RemoteTransform2D= get_node_or_null(String(remote_b_parent_path) + "/" + remote_b_name)
+	#if b_remote:
+		#remote_trans_b = b_remote
+	#else :
+		#remote_trans_b = _innit_remote_trans(remote_b_parent_path)
+		#remote_trans_b.name = remote_b_name
+
+
+func _enter_tree() -> void:
+	#remove
+	var f_parent: Node= get_node_or_null(remote_f_parent_path)
+	if f_parent: f_parent.add_child(remote_f)
 	
-	await object.ready
-	
-	var f_remote: RemoteTransform2D= get_node_or_null(String(remote_f_parent_path) + "/" + remote_f_name)
-	if f_remote:
-		remote_trans_f= f_remote
-	else :
-		remote_trans_f = _innit_remote_trans(remote_f_parent_path)
-		remote_trans_f.name = remote_f_name
-	
-	var b_remote: RemoteTransform2D= get_node_or_null(String(remote_b_parent_path) + "/" + remote_b_name)
-	if b_remote:
-		remote_trans_b = b_remote
-	else :
-		remote_trans_b = _innit_remote_trans(remote_b_parent_path)
-		remote_trans_b.name = remote_b_name
+	var b_parent: Node= get_node_or_null(remote_b_parent_path)
+	if b_parent: b_parent.add_child(remote_b)
+
+
+func _exit_tree() -> void:
+	remote_f.get_parent().remove_child(remote_f)
+	add_child(remote_f)
+	remote_b.get_parent().remove_child(remote_b)
+	add_child(remote_b)
+
 
 #### INHERITENCE ####
 
@@ -61,8 +78,8 @@ func enter() -> void:
 func exit() -> void:
 	super.exit()
 	
-	remote_trans_f.remote_path = ""
-	remote_trans_b.remote_path = ""
+	remote_f.remote_path = ""
+	remote_b.remote_path = ""
 	
 	gauge.visible = false
 
@@ -89,7 +106,4 @@ func _on_dynamic_timer_timeout() -> void:
 func _on_hit_box_hit(_damage: float, _hurt_box: HurtBox) -> void:
 	if is_current_state():
 		dynamic_timer.add_time(bonus_time_amount)
-
-
-
 
