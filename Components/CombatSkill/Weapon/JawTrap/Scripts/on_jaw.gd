b1445f97f8abd97df34ca40ca0aad7d665736096
Bannivisible <josseswan4@gmail.com>
2026-03-20 08:21:34 +0100

    charge anim

--- Components/CombatSkill/Weapon/JawTrap/Scripts/on_jaw.gd
+++ Components/CombatSkill/Weapon/JawTrap/Scripts/on_jaw.gd
@@ -31,29 +31,14 @@ func _ready() -> void:
 	
 	dynamic_timer.timeout.connect(_on_dynamic_timer_timeout)
 	hit_box.hit.connect(_on_hit_box_hit)
-	#
-	#await object.ready
-	#
-	#var f_remote: RemoteTransform2D= get_node_or_null(String(remote_f_parent_path) + "/" + remote_f_name)
-	#if f_remote:
-		#remote_trans_f= f_remote
-	#else :
-		#remote_trans_f = _innit_remote_trans(remote_f_parent_path)
-		#remote_trans_f.name = remote_f_name
-	#
-	#var b_remote: RemoteTransform2D= get_node_or_null(String(remote_b_parent_path) + "/" + remote_b_name)
-	#if b_remote:
-		#remote_trans_b = b_remote
-	#else :
-		#remote_trans_b = _innit_remote_trans(remote_b_parent_path)
-		#remote_trans_b.name = remote_b_name
 
 
 func _enter_tree() -> void:
-	#remove
+	remove_child(remote_f)
 	var f_parent: Node= get_node_or_null(remote_f_parent_path)
 	if f_parent: f_parent.add_child(remote_f)
 	
+	remove_child(remote_b)
 	var b_parent: Node= get_node_or_null(remote_b_parent_path)
 	if b_parent: b_parent.add_child(remote_b)
 
--- Scene/Characters/PlayableCharacter/Snake/snake.gd
+++ Scene/Characters/PlayableCharacter/Snake/snake.gd
@@ -1,7 +1,11 @@
 extends Character
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $AnimationTree
 
 func _input(_event: InputEvent) -> void:
+	var playback: AnimationNodeStateMachinePlayback= animation_tree.get("parameters/StateMachine/playback")
 	if Input.is_action_just_pressed("up"):
-		animation_player.play("Charge")
+		playback.travel("ToCharge")
+	if Input.is_action_just_pressed("down"):
+		playback.travel("ToChargeBackward")
